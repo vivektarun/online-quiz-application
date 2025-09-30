7ccde89de1898058628e594f0f6724914fee1854
vivektarun <vivektarun1234@gmail.com>
2025-09-30 16:26:02 +0530

    feat: add hasmany relation to quiz model

--- src/models/quiz.js
+++ src/models/quiz.js
@@ -1,16 +1,12 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Quiz extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Quiz.hasMany(models.Question, { foreignKey: 'quizId', as: 'questions', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+      Quiz.hasMany(models.Submission, { foreignKey: 'quizId', as: 'submissions', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
     }
   }
   Quiz.init({
