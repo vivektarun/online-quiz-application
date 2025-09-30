1786e4771f0fb548d2e0d0929fb90cf9dca005d3
vivektarun <vivektarun1234@gmail.com>
2025-09-30 14:52:47 +0530

    feat: add answers model and its migration

---
+++ src/models/answer.js
@@ -0,0 +1,36 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Answer extends Model {
+    static associate(models) {
+      Answer.belongsTo(models.Question, { foreignKey: 'questionId', as: 'question' });
+    }
+  }
+  
+  Answer.init({
+    questionId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    text: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'Answer text is required' },
+        notEmpty: { msg: 'Answer text cannot be emtpy' }
+      }
+    },
+    isCorrect: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Answer',
+    tableName: 'Answers',
+    timestamps: true
+  });
+  return Answer;
+};
