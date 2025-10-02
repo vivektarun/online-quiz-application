1f6c9bd9aa6a08d8352a91cd8ff34043e8181eab
vivektarun <vivektarun1234@gmail.com>
2025-10-02 19:29:03 +0530

    feat: remove tight coupling bw question service and curdreposiory

--- src/repositories/QuestionRepository.js
+++ src/repositories/QuestionRepository.js
@@ -1,9 +1,44 @@
 const CrudRepository = require("./CrudRepository");
-const { Question } = require('../models');
+const { Question, Answer } = require('../models');
 
 class QuestionRepository extends CrudRepository {
     constructor() {
         super(Question);
+    }
+
+    // Encapsulate fetching questions with their answers by quizId
+    async findAllWithAnswersByQuizId(quizId) {
+        const whereClause = {};
+        if (quizId) whereClause.quizId = quizId;
+
+        return await this.model.findAll({
+            where: whereClause,
+            include: [
+                {
+                    model: Answer,
+                    as: 'answers',
+                    attributes: ['id', 'text'],
+                },
+            ],
+            order: [
+                ['id', 'ASC'],
+                [{ model: Answer, as: 'answers' }, 'id', 'ASC'],
+            ],
+        });
+    }
+
+    // Fetch single question with answers by id (used after creation)
+    async findByIdWithAnswers(id, transaction = null) {
+        return await this.model.findByPk(id, {
+            include: [
+                {
+                    model: Answer,
+                    as: 'answers',
+                    attributes: ['id', 'text', 'isCorrect'],
+                },
+            ],
+            transaction
+        });
     }
 }
 
